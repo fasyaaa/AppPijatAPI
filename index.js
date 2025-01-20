4a54b22cf6d7f417828aa2f5b6e65b6d4ca7f637
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-01-21 01:33:31 +0700

    FIX - add library

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const app = express;
+const app = express();
 const port = 3000;
 
 const cors = require("cors");
@@ -23,5 +23,5 @@ app.use("/api/sesi", sesiRouter);
 
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`App listening at http://localhost:${port}`);
 });
